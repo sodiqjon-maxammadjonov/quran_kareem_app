20a421918f644d22cba374a6aa786a4d0466b102
sodiqjon-maxammadjonov <panda281106@gmail.com>
2025-07-22 18:25:07 +0500

    app coolors added

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color primary = Color(0xFF0D7377);
+  static const Color secondary = Color(0xFFFFD700);
+
+  static const Color lightGradientStart = Color(0xFFF8F9FF);
+  static const Color lightGradientEnd = Color(0xFFE8F5E8);
+
+  static const Color darkGradientStart = Color(0xFF1A1A2E);
+  static const Color darkGradientEnd = Color(0xFF16213E);
+
+  static const Color arabicText = Color(0xFF2C3E50);
+  static const Color uzbekText = Color(0xFF34495E);
+
+  static const Color lightBackground = Color(0xFFFFFFFF);
+  static const Color lightCard = Color(0xFFFFFFFF);
+  static const Color lightText = Color(0xFF2C3E50);
+  static const Color lightIcon = Color(0xFF333333);
+
+  static const Color darkBackground = Color(0xFF1A1A2E);
+  static const Color darkCard = Color(0xFF16213E);
+  static const Color darkText = Color(0xFFFFFFFF);
+  static const Color darkIcon = Color(0xFFF1F1F1);
+
+  static const Color grey = Color(0xFF9E9E9E);
+  static const Color lightGrey = Color(0xFFBDBDBD);
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+}
